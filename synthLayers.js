61a8617950d73dc28bb8410dd4e401b440c6fa80
Cory Nelson <fullnelsontech@gmail.com>
2025-09-04 13:02:59 -0400

    small progress

--- synthLayers.js
+++ synthLayers.js
@@ -2,14 +2,21 @@
 
 setcpm(100/4 )
 
+let drums =
+  stack(s("lt, bd bd*4").bank("bossdr550"),
+       s("~ sd,hh*8").bank("akaixr10").lpf("5000"))
 
 
+let synth =
 stack (
 note("<d3 a2 d2 g2>").layer(
-x=>x.s("z_sawtooth").vib(4).lpf(2000),
+x=>x.s("z_sawtooth").vib(4).lpf("2000"),
 x=>x.s("pulse").add(note(9))
-).scope(),
+),
 
 note("d5 f5 a5 [d3, g3]")
-.sound("piano, gm_electric_guitar_muted")
+.s("piano, gm_electric_guitar_muted")
 )
+
+stack(drums,arrange([4,synth])
+  )
