ef4c282175752c207ea7b031b483ad6865c18106
Cory Nelson <fullnelsontech@gmail.com>
2025-09-10 11:32:49 -0400

    update arrange to play multiple patterns

--- arrangeTest.js
+++ arrangeTest.js
@@ -1,40 +1,30 @@
-cpm(120/4)
+setcpm(120/4)
 
-let f1 =
+  let f1 =
   stack(
-    n(rand.range(0,12).segment(6))
-      .scale("F:minor")
-      .s("gm_tremolo_strings")
-      .gain(0.54)
-      .lpf("<200 500 1000 2000 4000 6000>")
-      ._scope()
-  )
+   n(rand.range(0,12).segment(6))
+.scale("F:minor")
+.s("gm_music_box").gain(.54).lpf("<200 300 500 1000 4000 6000 9000>")._scope())
 
-let f2 =
+  let f2 =
   stack(
-    n(rand.range(0,12).segment(1))
-      .scale("D:minor")
-      .s("gm_tremolo_strings")
-      .gain(0.54)
-      .cutoff(sine.range(800,8000))
-  )
+     n(rand.range(0,12).segment(6))
+.scale("D:minor")
+.s("gm_music_box").gain(.54).lpf(400).lpenv(4))
 
-let f3 =
+  let f3 =
   stack(
     n(rand.range(0,12).segment(4))
-      .scale("D:minor")
-      .s("gm_music_box")
-      .lpf(2000)
-      .lpq("<0 10 20 30>")
-  )
+.scale("D:minor")
+.s("gm_music_box").lpf(2000).hpf("<0 10 20 30 >"))
 
-let f4 =
+  let f4 =
   stack(
-    n(rand.range(0,12).segment(1))
-      .scale("D:minor")
-      .s("gm_tremolo_strings")
-      .gain(0.54)
-      .cutoff(sine.range(800,8000))
-  )
+   n(rand.range(0,12).segment(1))
+.scale("D:minor")
+.s("gm_music_box").gain(.54).cutoff(sine.range(800,8000)))
 
-arrange([ [1,f1], [1,f2], [1,f3], [1,f4] ])
+  
+
+//to have multiple patterns play in arrange. use stack
+arrange([7,stack(f1,f2)],[7,f3])
