e66fe8c9444f970cc875a039af122da3d7176c86
Cory Nelson <fullnelsontech@gmail.com>
2025-09-10 12:04:34 -0400

    new test to try out other effects

---
+++ otherEffectTest.js
@@ -0,0 +1,11 @@
+setcpm(130/4)
+
+ stack(
+  s("[bd sd]*2,hh*8")
+.cutoff(sine.range(500,6000)).bank("akaixr10"),
+
+
+ note("d d d# d".fast(2)).s("sawtooth").tremolosync("4").tremoloskew("<1 .5 0>")
+)
+
+
