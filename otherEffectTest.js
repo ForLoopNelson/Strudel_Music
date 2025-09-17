faa4ad49646be38e81cfcf4ecfc4726d84bfb9cc
Cory Nelson <fullnelsontech@gmail.com>
2025-09-17 11:38:21 -0400

    synth1 is good synth2 not so much WIP

--- otherEffectTest.js
+++ otherEffectTest.js
@@ -1,11 +1,20 @@
 setcpm(170/8)
 
+const drums =
  stack(
   s("[bd sd]*2,hh*8")
-.cutoff(sine.range(500,6000)).bank("akaixr10"),
+.cutoff(sine.range(500,6000)).bank("akaixr10"))
 
-
+const synth1 = 
+  stack(
  note("d d d# d".fast(2)).s("sawtooth").tremolosync("4").tremoloskew("<1 .5 0>"),
- note("f2 f2 f2# f3".fast(2)).s("gm_electric_bass_finger")
+ note("f2 f2 f# f3".fast(2)).s("gm_electric_bass_finger")
 )
 
+const synth2 = 
+  stack(
+    note("d1 f# g2 d2 ").fast(2).s("sqr")
+  )
+
+
+stack(drums,arrange([4,synth1],[4,synth2]))
