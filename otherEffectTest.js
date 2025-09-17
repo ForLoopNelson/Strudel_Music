5d8b1ab55f2757457fb3866ef9b87a143286a579
Cory Nelson <fullnelsontech@gmail.com>
2025-09-17 11:25:16 -0400

    work in progress add chords and other

--- otherEffectTest.js
+++ otherEffectTest.js
@@ -1,11 +1,11 @@
-setcpm(130/4)
+setcpm(170/8)
 
  stack(
   s("[bd sd]*2,hh*8")
 .cutoff(sine.range(500,6000)).bank("akaixr10"),
 
 
- note("d d d# d".fast(2)).s("sawtooth").tremolosync("4").tremoloskew("<1 .5 0>")
+ note("d d d# d".fast(2)).s("sawtooth").tremolosync("4").tremoloskew("<1 .5 0>"),
+ note("f2 f2 f2# f3".fast(2)).s("gm_electric_bass_finger")
 )
-
 
