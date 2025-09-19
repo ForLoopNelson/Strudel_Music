89f08c73945482a0ba90cc56784802b906e00c0d
Cory Nelson <fullnelsontech@gmail.com>
2025-09-19 13:16:47 -0400

    updated

--- polymeterTest.js
+++ polymeterTest.js
@@ -1,11 +1,15 @@
 setGainCurve(x => Math.pow(x,2))
 
 
-setCps(150/60/4)
+setCps(140/60/4)
 
 
-$:stack(
-   s("jazz").struct("1 1 0 [0 1] 0 1 1 0").lpf(4000).distort("2").gain(0.8).pan(1),
-   s("bd hh sd ").lpf(2000).distort("3"),
+DRUMS:stack(
+   s("<bd sd, hh ~ hh>*4").bank("bossdr110").lpf(2000).distort("1").gain(0.7),
+   s("bd hh sd ").lpf(1000).distort("2"))
+
+POLY:stack(
    polymeter("c eb g", "c2 g2").note().gain(1.50),
    polymeter("c3 f3 g3 ab3 eb3 g4", "c3 ab3").note().gain(1.20))
+
+PAD:stack(note("c2 f3 [c4 eb4 g4]").add(note(perlin.range(0,.5))).clip(2).s("gm_fx_crystal").release("<2 1 .5 .25>"))
