66672a202b2714f1854b5b9df88907a036efb2dc
Cory Nelson <fullnelsontech@gmail.com>
2025-09-21 13:05:49 -0400

    first attempt at break beats and using more complex syntax

---
+++ breakBeats.js
@@ -0,0 +1,15 @@
+//attempt to learn break beats from watching clips of switch angel
+
+setGainCurve(x => Math.pow(x,2))
+
+
+setCpm(170/4)
+
+
+$drums:stack(s("bd:1").beat("0,7?,10",16).duck("3"),
+             s("sd:2").beat("4,12",16),
+             s("hh:4!8").bank("akaimpc60").orbit(2).spiral({steady: .75}))
+
+
+$bass: stack(s("supersaw!8").note("<c# f d# [d# a#2]>/2".sub(12)).orbit(3)._scope())
+   
