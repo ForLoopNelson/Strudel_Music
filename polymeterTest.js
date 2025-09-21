15b563512933645f9d40a71f2aa63a265e019a2f
Cory Nelson <fullnelsontech@gmail.com>
2025-09-21 12:40:07 -0400

    change names from all upper to python2.7 and added epiano

--- polymeterTest.js
+++ polymeterTest.js
@@ -4,12 +4,14 @@ setGainCurve(x => Math.pow(x,2))
 setCps(140/60/4)
 
 
-DRUMS:stack(
+
+$drums:stack(
    s("<bd sd, hh ~ hh>*4").bank("bossdr110").lpf(2000).distort("1").gain(0.7),
    s("bd hh sd ").lpf(1000).distort("2"))
 
-POLY:stack(
-   polymeter("c eb g", "c2 g2").note().gain(1.50),
+$poly:stack(
+   polymeter("c eb g", "c2 g2 bb2 c4").note().gain(1.50),
    polymeter("c3 f3 g3 ab3 eb3 g4", "c3 ab3").note().gain(1.20))
 
-PAD:stack(note("c2 f3 [c4 eb4 g4]").add(note(perlin.range(0,.5))).clip(2).s("gm_fx_crystal").release("<2 1 .5 .25>"))
+$pad:stack(note("c2 f3 g3 [c4 eb4 g4]").add(note(perlin.range(0,.5))).clip(2).s("gm_cello").release("<2 1 .5 .25>"),
+           note("[eb4 g4] c3 g2 c2").add(note(perlin.range(0, .8))).clip(1).s("gm_epiano1"))
