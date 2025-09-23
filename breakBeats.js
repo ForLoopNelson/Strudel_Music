d7752bb8dc6765590223e4bd5ced72cd53a9a1a2
Cory Nelson <fullnelsontech@gmail.com>
2025-09-23 13:09:52 -0400

    add sample to bass stack

--- breakBeats.js
+++ breakBeats.js
@@ -11,5 +11,5 @@ $drums:stack(s("bd:1").beat("0,7?,10",16).duck("3"),
              s("hh:4!8").bank("akaimpc60").orbit(2).spiral({steady: .75}))
 
 
-$bass: stack(s("supersaw!8").note("<c# f d# [d# a#2]>/2".sub(12)).orbit(3)._scope())
-   
+$bass: stack(s("supersaw!8").note("<c# f d# [d# a#2]>/2".sub(12)).orbit(3).pan(.5)._scope(),
+             n(rand.range(0,12).segment(8)).scale("E:major").s("gm_fx_rain").pan(.9).gain(1.25))
