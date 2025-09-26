e047ee042f1aad639f8c4b4bd2fbb6cc50510b7b
Cory Nelson <fullnelsontech@gmail.com>
2025-09-26 12:47:28 -0400

    change to let and start arrange

--- voiceSynths.js
+++ voiceSynths.js
@@ -8,9 +8,11 @@ setCpm(110/4)
 
 // $bass:
 
-$synth: stack(note("<e4 e5 c#4 b4>").s("gm_pad_halo"),
+let synth = stack(note("<e4 e5 c#4 b4>").s("gm_pad_halo"),
               note("[e2, a] b2 e@3 a#2 e /2").s("gm_pad_sweep"),
               note("<e2 e2 a#3 b3 c#2>").s("gm_pad_bowed"))
 
-$voice: stack(note("e e f#3 a# c4 e4").fast(1).s("gm_voice_oohs"),
+let voice = stack(note("e e f#3 a# c4 e4").fast(1).s("gm_voice_oohs"),
               note("[e g# b] e3 a#4").s("gm_synth_choir"))
+
+arrange([2,stack(voice,bass)],[4,stack(drums,voice,synth)])
