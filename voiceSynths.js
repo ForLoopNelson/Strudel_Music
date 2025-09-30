038047451edf86f9c7210adc7dd3e1bf651397f0
Cory Nelson <fullnelsontech@gmail.com>
2025-09-30 13:03:09 -0400

    add pan and pitchwheels

--- voiceSynths.js
+++ voiceSynths.js
@@ -13,16 +13,16 @@ let drums =
 
 let bass = 
     stack(note("e3 g#3 d#3 [a4, c#4]").sound("gm_cello, gm_synth_bass_2"),
-              note("e!2 f#3 a# c4 e4!2 d#4").fast(2).rev().s("gm_voice_oohs"))
+              note("e!2 f#3 a# c4 e4!2 d#4").fast(2).rev().s("gm_voice_oohs").color("#347deb").pitchwheel())
 
 let synth = 
     stack(note("<e4 e5 c#4 b4>").s("gm_pad_halo"),
-              note("[e2, a, c2]/2 b2 e@3 a#2 e /2").s("gm_pad_sweep"),
-              note("<e2 e2 a#3 b3 c#2>").s("gm_pad_bowed"))
+              note("[e2, a, c2]/2 b2 e@3 a#2 e /2").s("gm_pad_sweep").pan(.7),
+              note("<e2 e2 a#3 b3 c#2>").s("gm_pad_bowed").pan(1))
 
 let voice = 
-    stack(note("e!2 f#3 a# c4 e4!2 d#4").fast(1).s("gm_voice_oohs"),
-              note("[e g# b] e3 a#4").s("gm_synth_choir"))
+    stack(note("e!2 f#3 a# c4 e4!2 d#4").fast(1).s("gm_voice_oohs").color("#c8afed").pitchwheel(),
+              note("[e g# b] e3 a#4").s("gm_synth_choir").pan(.35))
 
 let voice2 = note("e g# b e4 c#5 d#4").s("gm_pad_choir")
 
