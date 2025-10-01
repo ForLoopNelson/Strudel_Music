33ad5e9856c59f246fc9062c3231474b97e7e947
Cory Nelson <fullnelsontech@gmail.com>
2025-10-01 10:51:26 -0400

    update tempo and changed notes

--- miniNotation.js
+++ miniNotation.js
@@ -1,11 +1,12 @@
-setcpm(130/4 )
+setcpm(110/4 )
 
 
 note(`<[c4 [f#4 a4] d3 [b3 c4]]
-       [e3 [g3 b3] c4 e4 ~ c3 [d3 f#3]]
-       [c3! [f#3 ~ a4] d4 [b4 e3 c4] c4]
+       [e3 [g3 b3 a4] g4 ~ c3 [d3 f#3]]
+       [g3 f#5 ~ [a4 d4 b4] [e3 c4] c4]
        [[c3/2]],
         
-       [e4 [c3 c4] c3 a2]
-       [g3 [c3 e4] f#3 ~ c3 [g3 b4]]
-       [c5! [a4 ~ c2] f#3! [c3! b2 c5]]>`).s("piano")
+       [e4 [c3 c4] c3 a2]*2
+       [g3 c3 [e4] [g3 b4]]
+       [d2! [a4 ~ c2] f#3!]
+       [g3/2]>`).s("piano")
