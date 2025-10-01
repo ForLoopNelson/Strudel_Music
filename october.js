7deef64d6f86088ecef2f9a158319aa7e0634aa9
Cory Nelson <fullnelsontech@gmail.com>
2025-10-01 12:05:38 -0400

    add more and change tempo

--- october.js
+++ october.js
@@ -1,7 +1,7 @@
 //it's spooky season. let's make a fun industrial synth jam!
 
 
-setcpm(150/4)
+setcpm(100/4)
 
 let drums =
 stack(s("bd:3").beat("0,7?,10",16).duck("3"),
@@ -9,11 +9,20 @@ stack(s("bd:3").beat("0,7?,10",16).duck("3"),
              s("hh:6").beat("0,2,4,5?,6,8?",32).bank("akaimpc60"))
 
 
+
 let tone =
-stack(note("d2!2 f#4 d2 d3 d2 d3 d2").s("gm_synth_bass_1"))
+stack(note("d2!2 f#4 d2 d3 d2 d3 d2").s("gm_synth_bass_1"),
+      note("d3/2 g2 b2 c#3 d3").s("supersaw"))
+
+let pulse =
+stack(note("d5 f#2 c#4 d4 d3 g4 d5 e5 d4 e4 c#3 ").s("pulse").fast(4).gain(.45))
 
 
 
+
+
+
+stack(drums,arrange([4,stack(tone,pulse)]))
 
 
 
