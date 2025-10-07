12b80da02d811cf71a757a8a9e4bb9b46b5ff4dc
Cory Nelson <fullnelsontech@gmail.com>
2025-10-07 11:55:51 -0400

    add some panning and decay/sustain

--- nightmareOnSynthSt.js
+++ nightmareOnSynthSt.js
@@ -4,8 +4,9 @@ let noise = stack(note("ab cb eb gb ab bb db ab2@3").s("gm_pad_poly"),
                   note("ab2!3 eb2!3 db!3 cb!3").s("gm_pad_warm"))
 
 
-let highs = stack(note("ab4@3 cb4 db3 ab3@2 cb3@3").s("gm_glockenspiel"),
-                  note("ab4@3 db4!4 cb4@2").s("gm_synth_choir"))
+let highs = stack(note("ab4@3 cb4 db3 ab3@2 cb3@3").s("gm_glockenspiel").pan(.75),
+                  note("ab4@3 db4!4 cb4@2").s("gm_synth_choir"),
+                  note("ab2").s("gm_vibraphone").decay(.2).sustain("<.9>").pan(.44))
 
 
 
