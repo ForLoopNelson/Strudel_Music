6c5c7dac98f84a42708e5222115e7d52313ebd67
Cory Nelson <fullnelsontech@gmail.com>
2025-10-07 11:49:34 -0400

    spooky 80's movie vibe

---
+++ nightmareOnSynthSt.js
@@ -0,0 +1,16 @@
+setcpm(70/4)
+
+let noise = stack(note("ab cb eb gb ab bb db ab2@3").s("gm_pad_poly"),
+                  note("ab2!3 eb2!3 db!3 cb!3").s("gm_pad_warm"))
+
+
+let highs = stack(note("ab4@3 cb4 db3 ab3@2 cb3@3").s("gm_glockenspiel"),
+                  note("ab4@3 db4@4 cb4@1 ab5").s("gm_synth_choir"))
+
+
+
+
+
+
+
+stack(arrange([2,noise],[4,stack(noise,highs)]))
