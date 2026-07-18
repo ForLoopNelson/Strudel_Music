78e6b93efac2cf0b557d2cf3fbe9b96f3abc0513
Cory Nelson <fullnelsontech@gmail.com>
2026-07-18 11:05:21 -0400

    add more sounds

--- freqBells.js
+++ freqBells.js
@@ -1,3 +1,17 @@
 setcpm(100/4)
 
-freq("<200 [300,500] 400 [500,<600 670 712 670>]>*8").s("gm_glockenspiel")
+let synthFreq = stack(
+freq("<200 [300,500] 400 [500,<600 670 712 670>]>*8").s("gm_glockenspiel"),
+freq("<600 [300,200] 600 [800,<300 450 650 720>]>*4").s("gm_shakuhachi"))
+
+
+let organs = stack(
+  freq("<350 [375] 320 [500 790]>*8").s("gm_percussive_organ")
+)
+
+
+
+
+
+
+arrange([4,stack(synthFreq,organs)])
