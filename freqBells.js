34999d130b573ddb707f64044cd8c41f625ceaa2
Cory Nelson <fullnelsontech@gmail.com>
2026-07-27 11:24:01 -0400

    update drum remove linger, add crush and distort

--- freqBells.js
+++ freqBells.js
@@ -1,8 +1,8 @@
 setcpm(100/4)
 
 let drum = stack(s("bd:1").beat("0,4?,10",16).duck("3"),
-             s("sd:2").beat("2,8?,12",16),
-             s("hh:12!16").linger("<.15 .1 .12>").bank("akaimpc60").orbit(4)
+             s("sd:2").beat("2,6?,12",16).distort("5: .3"),
+             s("hh:12!16").crush("<7 8>").bank("akaimpc60").orbit(4)
   
 )
 
