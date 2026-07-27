c870bc52164c58402b55f19acbb31b0302dbfe0e
Cory Nelson <fullnelsontech@gmail.com>
2026-07-27 11:19:55 -0400

    add synthBreak and update arrange

--- freqBells.js
+++ freqBells.js
@@ -26,7 +26,14 @@ let bass = stack(
   freq("<300 [500 540] 470 500 >*4").s("gm_lead_8_bass_lead").gain(.48).pan(.78).transpose("<1 -2 0 2>")
 )
 
+let synthBreak = stack(
+  freq("<600 [300,200] 600 [800,<300 450 650 720>]>*4").s("z_sine").seg(16).lpf(tri.range(300, 3000).slow(3)).gain(.45).pan(.49)
+)
 
 
 
-arrange([2,stack(synthFreq,organs)],[4,stack(bass,synthFreq,organs,drums)])
+
+
+arrange([2,stack(synthFreq,organs)],
+        [4,stack(bass,synthFreq,organs,drums)],
+        [2,stack(bass.crush("<4 7>"),synthBreak.transpose("<-1 2 0 1>"), drum)])
