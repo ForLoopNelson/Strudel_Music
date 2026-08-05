2997de3830217981e9b550351f90df35481cec0a
Cory Nelson <fullnelsontech@gmail.com>
2026-08-05 11:29:37 -0400

    first commit, simple synth line crushed

---
+++ crushedSynth.js
@@ -0,0 +1,10 @@
+setcpm(80/4)
+
+let drums =
+stack(s("bd:3").beat("0,7?,10",16).duck("3"),
+             s("sd:8").beat("4,12",16),
+             s("hh:6").beat("0,2,4,5?,6,8?",32).bank("akaimpc60"))
+
+let synth = stack(note("a3!3 c3 e3 f3").s("gm_synth_bass_2").seg(16).lpf("2000").crush("<3.8>"))
+
+arrange([4,stack(drums,synth)])
